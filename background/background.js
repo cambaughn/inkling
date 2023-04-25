23a67b490cda195e85652b6ab904b97713cc9fc4
Cameron Baughn <cam.baughn@gmail.com>
2023-04-25 16:11:57 -0700

    Get video data only once per page load

--- background/background.js
+++ background/background.js
@@ -1,23 +1,17 @@
 import { getVideoId, getYoutubeData } from "./util";
 console.log('background script ======');
 
-// Listen for page change to get video information
+let pageLoaded = false;
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  // Wait for the page to complete loading so we only call this once
-    if (changeInfo.status === 'complete' && tab.url.includes('youtube.')) {
-      chrome.tabs.sendMessage(tabId, { type: 'videoData', data: null });
-
-      // Get the video description details
-      console.log('sending message to refresh description');
-      chrome.tabs.sendMessage(tabId, { type: 'refreshDescription' });
-
-      // Get the videoId
-      let videoId = getVideoId(tab.url);
-      // get the video data
-      getYoutubeData(videoId).then((videoData) => {
-        // send the data to the content script
-        chrome.tabs.sendMessage(tabId, { type: 'videoData', data: videoData });
-      });
-    }
+  if (changeInfo.status === 'complete' && tab.url.includes('youtube.') && !pageLoaded) {
+    pageLoaded = true;
+    chrome.tabs.sendMessage(tabId, { type: 'videoData', data: null });
+    chrome.tabs.sendMessage(tabId, { type: 'refreshDescription' });
+    let videoId = getVideoId(tab.url);
+    getYoutubeData(videoId).then((videoData) => {
+      chrome.tabs.sendMessage(tabId, { type: 'videoData', data: videoData });
+      pageLoaded = false;
+    });
   }
-);
+});
