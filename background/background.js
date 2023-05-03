b25aff3fc8c48cf0cf4983f6305eadeb9d299688
Cameron Baughn <cam.baughn@gmail.com>
2023-05-02 23:32:05 -0700

    Fix tab update

--- background/background.js
+++ background/background.js
@@ -5,7 +5,7 @@ let videoId = null;
 let mounted = false;
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  console.log('testing tab update =====', tab.url);
+  console.log('testing tab update =====', tab.url, changeInfo.status);
   if (changeInfo.status === 'complete' && tab.url.includes('youtube.')) {
     videoId = getVideoId(tab.url);
     console.log('get video id =====', videoId);
--- content-script/util/openAI.js
+++ content-script/util/openAI.js
@@ -12,60 +12,16 @@ const getSummary = async (videoDetails, subtitles) => {
     const videoTitle = videoDetails?.snippet?.title;
     const videoDescription = videoDetails?.snippet?.description;
 
-    console.log("Let's begin summary creation");
+    const prompt = `Please provide a 2-3 sentence summary of the main topic of the video, followed by bullet-pointed list of the main points. - Title: ${videoTitle}, - Description: ${videoDescription} - Transcript: ${subtitles}`;
 
-    // Split the subtitles into an array of snippets no longer than 15k characters
-    const MAX_LENGTH = 14000;
-    subtitles = `Title: ${videoTitle} \n Transcript: ${subtitles}`;
-    let snippets = subtitles.match(new RegExp(`.{1,${MAX_LENGTH}}`, "g"));
-
-    console.log(`Splitting subtitles into ${snippets.length} snippets`);
-
-    // Send each snippet to the OpenAI API and get summaries
-    let summaries = [];
-    for (let i = 0; i < snippets.length; i++) {
-      let snippet = snippets[i];
-      let messages = [
-        {
-          role: 'system',
-          content:
-            "You're a research assistant designed to help users get the most important content out of videos in the shortest time possible. These videos can consist of a title, description, and transcript.",
-        },
-        { role: 'user', content: `Please provide a short summary of this section of the video transcript. \n` + snippet },
-      ];
-
-      console.log(`Sending snippet ${i+1} to OpenAI API`);
-
-      const openAiResponse = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages
-      });
-
-      const responseText = openAiResponse?.data?.choices[0].message?.content;
-      summaries.push(responseText);
-    }
-
-    console.log(`Received ${summaries.length} summaries`);
-
-    // Combine all summaries into a smaller summary of the entire thing
-    let fullSummary = '';
-    for (let i = 0; i < summaries.length; i++) {
-      fullSummary += `${i+1}. ${summaries[i]}\n`;
-    }
-
-    console.log(`Combined all summaries into full summary`);
-
-    // Send the full summary to the OpenAI API to get a shorter summary
-    let messages = [
+    const messages = [
       {
         role: 'system',
         content:
-          "You're a research assistant designed to help users get the most important content out of videos in the shortest time possible. These videos can consist of a title, description, and transcript.",
+          "You're a research assistant designed to help users get the most important content out of videos in the shortest time possible. These videos can consist of a title, description, and transcript. Be aware that the description can contain product links, advertisements, sponsors, and other content that isn't directly relevant to the content of the video itself. Use it to educate yourself about the content of the video, but don't hold it in high regard, and don't return any sponsored or irrelevant content.",
       },
-      { role: 'user', content: `Please provide a shorter summary of the entire video, followed by a bullet-pointed list of the main points: \n` + fullSummary },
+      { role: 'user', content: prompt },
     ];
-
-    console.log(`Sending full summary to OpenAI API`);
 
     const openAiResponse = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
@@ -74,18 +30,11 @@ const getSummary = async (videoDetails, subtitles) => {
 
     const responseText = openAiResponse?.data?.choices[0].message?.content;
 
-    console.log(`Received final summary`);
-
     return responseText;
   } catch (error) {
-    if (error.response) {
-      console.log(error.response.status);
-      console.log(error.response.data);
-    } else {
-      console.log(error.message);
-    }
+    console.error(error);
     return null;
   }
 };
 
-export { getSummary };
+export { getSummary }
