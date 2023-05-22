96f72584a1daa6950a6af407ff60de8e0b01ea8c
Cameron Baughn <cam.baughn@gmail.com>
2023-05-22 12:20:18 -0700

    Try to enable popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,18 @@
 /// <reference types="chrome" />
 /// <reference types="vite-plugin-svgr/client" />
 
-import Logo from "./Logo";
 import "./App.css";
-import Content from "../content-script/src/Content.jsx";
+
 
 function App() {
-  const active = 'content';
-
-  if (import.meta.env.MODE === 'dev' && active === 'content') {
-    return (
-      <div className="devContentWrapper">
-        <Content videoId="nJslrTT-Yhc" />
-      </div>
-    )
-  } else {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Logo className="App-logo" id="App-logo" title="React logo" />
-          <p>Hello, World!</p>
-          <p>I'm a Chrome Extension Popup!</p>
-        </header>
-      </div>
-    );
-  }
-
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello, World!</p>
+        <p>I'm a Chrome Extension Popup!</p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
